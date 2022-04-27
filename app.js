ddfe5c333b6b8e40346fc4135f1e0b71353c28f3
AnshumanDubey1999 <anshumandubey2k18@gmail.com>
2022-04-27 12:45:28 +0530

    Linked to DB

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI, () => {
+    console.log('DB Connected!')
+})
+
+mongoose.connection.on('error', (err) => {
+    console.log('Connection failed!', {err})
+})
 
 const routes = require('./routes/index');
 app.use('/', routes);
