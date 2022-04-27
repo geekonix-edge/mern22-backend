a0b7510f373964e084e24c0895e73db5f659d87d
AnshumanDubey1999 <anshumandubey2k18@gmail.com>
2022-04-27 12:33:08 +0530

    Created GET API

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    res.json({
+        success: true,
+        message: 'Hello from the / route!'
+    })
+})
+
+app.get('/curbrain', (req, res) => {
+    res.json({
+        success: true,
+        message: 'Hello from the /curbrain route!'
+    })
+})
+
+app.get('/geekonix', (req, res) => {
+    res.json({
+        success: true,
+        message: 'Hello from the /geekonix route!'
+    })
+})
+
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running!')
 })
